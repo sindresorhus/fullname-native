ebd11ae3702648c2635b5f9b4dc04354b6652e50
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-22 18:13:50 +0200

    Use `meow` in the CLI

--- cli.js
+++ cli.js
@@ -1,28 +1,14 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var fullname = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    fullname',
-		'    Sindre Sorhus'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  fullname',
+		'  Sindre Sorhus'
+	].join('\n')
+});
 
 console.log(fullname);
